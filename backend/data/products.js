474d07c08a18518d0ddfcb9eefa28f77e38d9f31
Bikalp Timalsina <48162138+Youngbikalp@users.noreply.github.com>
2021-03-04 13:53:22 -0800

    node, express and mongodb working

---
+++ backend/data/products.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    name: "2021 Toyota Prius",
+    image: "/images/Toyota_Prius.png",
+    description:
+      "The Prius is a power-split or series-parallel (full) hybrid, sometimes referred to as a combined hybrid, a vehicle that can be propelled by gasoline or electric power or both.",
+    brand: "Toyota",
+  },
+
+  {
+    name: "2021 RAV4",
+    image: "/images/Rav4.png",
+    description:
+      "The 2021 RAV4 uses a 203-hp 2.5-liter four-cylinder engine paired with an eight-speed automatic transmission. Front-wheel drive remains standard, but Toyota now offers a choice of all-wheel-drive systems, dependent on trim",
+    brand: "Toyota",
+  },
+];
+
+export default products;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import axios from "axios";
+const ProductScreen = ({ match }) => {
+  const [product, setProduct] = useState({});
+  //console.log(product);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [match.params.id]);
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
