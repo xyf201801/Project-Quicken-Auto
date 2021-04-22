933cd7228431aeb19827f336d8c7782afad57484
Bikalp Timalsina <48162138+Youngbikalp@users.noreply.github.com>
2021-04-21 19:42:25 -0700

    Revert "Revert "Added price and stock status""

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -30,10 +30,19 @@ const productSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-
     description: {
       type: String,
       required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
     },
   },
   {
