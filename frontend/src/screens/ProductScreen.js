35c3de8b1eb4275989f83e49c98f1c46c4628a98
Bikalp Timalsina <48162138+Youngbikalp@users.noreply.github.com>
2021-04-23 02:50:00 -0700

    Cart Page, Add to cart and delete from cart functionality

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -9,12 +9,14 @@ import {
   Card,
   Button,
   ListGroupItem,
+  Form,
 } from "react-bootstrap";
 import { listProductsDetails } from "../actions/productActions";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 //import { productDetailsReducer } from "../reducers/productReducers";
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
+  const [qty, setQty] = useState(1);
   //console.log(product);
   const dispatch = useDispatch();
 
@@ -24,6 +26,12 @@ const ProductScreen = ({ match }) => {
     dispatch(listProductsDetails(match.params.id));
   }, [dispatch, match]);
 
+  // When clicking the add to cart it goes to the cart page ex:
+  // http://localhost:3000/cart/607e86a89a8b94ad08b4062a?qty=3
+  //local host, then cart, the product id and the quality
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
+  };
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
@@ -69,8 +77,32 @@ const ProductScreen = ({ match }) => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                {/* if the item is in stock then show the following */}
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Days</Col>
+                      <Col>
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {/* if lets say count in stock is 5 then the code: [...Array(product.countInStock).keys()] */}
+                          {/* is setting the values to [0,1,2,3,4,5] */}
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCartHandler}
                     className="btn-block"
                     type="button"
                     disabled={product.countInStock === 0}
