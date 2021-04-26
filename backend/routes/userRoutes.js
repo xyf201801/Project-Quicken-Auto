16b79e2ae4bc9fcfac0be2c4c96c2c0c60c765a9
Bikalp Timalsina <48162138+Youngbikalp@users.noreply.github.com>
2021-04-26 12:56:48 -0700

    imported routed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
 
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
-//
 router.post("/login", authUser);
+//protecting the getUserProfile
+//can use protect whereever we want to protect a route
+router.route("/profile").get(protect, getUserProfile);
 
+router.route("/").post(registerUser);
 export default router;
