f991feb477f8a71bd7284a1ebea11d86a7b820d5
Tony Xie <yifan.xie@sjsu.edu>
2021-04-28 13:24:54 -0700

    User Log in & Log out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -14,11 +15,13 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/" component={HomeScreen} exact />
+          <Route path="/login" component={LoginScreen} exact />
+
           <Route path="/product/:id" component={ProductScreen} exact />
           {/* here this is the cart route and since the it can be optional
           we put a ? at the end */}
           <Route path="/cart/:id?" component={CartScreen} exact />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
