e3d8e2b1e2f7e7511628ff30c1f09e03a319dc8e
Tony Xie <yifan.xie@sjsu.edu>
2021-05-16 00:39:06 -0700

    Payment Screen

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,18 +54,28 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+//Update order to paid
+//GET /api/orders/:id/pay
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
 
-// const getOrderById = asyncHandler(async (req, res) => {
-//   const order = await Order.findById(req.params.id).populate(
-//     'user',
-//     'name email'
-//   )
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
 
-//   if (order) {
-//     res.json(order)
-//   } else {
-//     res.status(404)
-//     throw new Error('Order not found')
-//   }
-// })
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,14 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 9000;
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`)
-);
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`));
 
 export { notFound, errorHandler };
